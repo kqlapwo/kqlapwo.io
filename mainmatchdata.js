62711039a7d5ff1b63e2ca0398c9fd5f8baae1d2
kqlapwo <104405234+kqlapwo@users.noreply.github.com>
2022-07-26 01:43:53 +0900

    Create mainmatchdata.js

---
+++ mainmatchdata.js
@@ -0,0 +1,16 @@
+export default [
+        {
+            "matchnumber": "1",
+            "hometeam": "토트넘",
+            "awayteam": "레인저스",
+            "homescore": "2",
+            "awayscore": "1"
+        },
+        {
+            "matchnumber": "2",
+            "hometeam": "첼시",
+            "awayteam": "리버풀",
+            "homescore": "2",
+            "awayscore": "0"
+        }
+    ]
